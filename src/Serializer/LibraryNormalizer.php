2ff6ffffd54c524389387847178c0a751c75806d
Christoph Reiter <reiter.christoph@gmail.com>
2020-08-05 15:05:05 +0200

    Move the normalizers into the bundles

---
+++ src/Serializer/LibraryNormalizer.php
@@ -0,0 +1,46 @@
+<?php
+namespace DBP\API\AlmaBundle\Serializer;
+
+use DBP\API\CoreBundle\Entity\Person;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
+
+class LibraryNormalizer implements ContextAwareNormalizerInterface, NormalizerAwareInterface
+{
+    use NormalizerAwareTrait;
+
+    private const ALREADY_CALLED = 'LDAP_PERSON_ATTRIBUTE_NORMALIZER_LIBRARY_ALREADY_CALLED';
+
+    /**
+     * @var Security
+     */
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function normalize($object, $format = null, array $context = [])
+    {
+        if ($this->security->isGranted('ROLE_F_BIB_F')) {
+            $context['groups'][] = 'role_library';
+        }
+
+        $context[self::ALREADY_CALLED] = true;
+
+        return $this->normalizer->normalize($object, $format, $context);
+    }
+
+    public function supportsNormalization($data, $format = null, array $context = [])
+    {
+        // Make sure we're not called twice
+        if (isset($context[self::ALREADY_CALLED])) {
+            return false;
+        }
+
+        return $data instanceof Person;
+    }
+}
