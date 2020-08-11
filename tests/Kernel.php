f8724c0a3821a6ada439dd965a17fc5d121a8142
Christoph Reiter <reiter.christoph@gmail.com>
2020-08-11 15:07:59 +0200

    Move the API tests into their respective bundles
    We create a dummy kernel and use that for running tests

---
+++ tests/Kernel.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBP\API\AlmaBundle\Tests;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle;
+use DBP\API\AlmaBundle\DbpAlmaBundle;
+use DBP\API\CoreBundle\DbpCoreBundle;
+use Nelmio\CorsBundle\NelmioCorsBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Bundle\SecurityBundle\SecurityBundle;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\RouteCollectionBuilder;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+
+    public function registerBundles(): iterable
+    {
+        yield new FrameworkBundle();
+        yield new SecurityBundle();
+        yield new TwigBundle();
+        yield new NelmioCorsBundle();
+        yield new ApiPlatformBundle();
+        yield new DbpCoreBundle();
+        yield new DbpAlmaBundle();
+    }
+
+    protected function configureRoutes(RouteCollectionBuilder $routes)
+    {
+        $routes->import($this->getProjectDir().'/../../config/routes/api_platform.yaml');
+    }
+
+    protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
+    {
+        $c->loadFromExtension('framework', [
+            'test' => true,
+        ]);
+    }
+}
---
+++ tests/Test.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DBP\API\AlmaBundle\Tests;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+class Test extends ApiTestCase
+{
+    /** @var Client */
+    protected $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->client = static::createClient();
+    }
+
+    public function testIndex()
+    {
+        $response = $this->client->request('GET', '/books/library_books');
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+    }
+
+    public function testJSONLD()
+    {
+        $response = $this->client->request('GET', '/books/library_books', ['headers' => ['HTTP_ACCEPT' => 'application/ld+json']]);
+        $this->assertEquals(Response::HTTP_UNAUTHORIZED, $response->getStatusCode());
+        $this->assertJson($response->getContent(false));
+    }
+}
