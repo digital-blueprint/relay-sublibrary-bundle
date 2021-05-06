0bdeb0e360eb8d17cbc412b51085e3f4ee333335
Christoph Reiter <reiter.christoph@gmail.com>
2021-05-06 14:11:54 +0200

    Port to cs-fixer 3.0

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,27 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+$config = new PhpCsFixer\Config();
+$config->setRules([
+    '@Symfony' => true,
+    '@PHP70Migration' => true,
+    '@PHP71Migration' => true,
+    '@PHP73Migration' => true,
+    '@DoctrineAnnotation' => true,
+    'doctrine_annotation_array_assignment' => ['operator' => '='],
+    'yoda_style' => false,
+    'strict_comparison' => true,
+    'strict_param' => true,
+    'declare_strict_types' => true,
+    'method_argument_space' => ['on_multiline' => 'ignore'],
+    'no_superfluous_phpdoc_tags' => false,
+    'phpdoc_align' => false,
+])
+->setRiskyAllowed(true)
+->setFinder($finder);
+
+return $config;
