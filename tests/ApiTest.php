6c67aa81425d523668f018bca26a8b4334bd8fb4
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2023-07-11 13:28:20 +0200

    core bundle update and adaptions; replaced deprecate apiplatform\core references

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\SublibraryBundle\Tests;
 
-use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
 
 class ApiTest extends ApiTestCase
 {
