646589adcb031aa83000b2890623baa73fcfe7c0
Tobias Gross-Vogt <tobias.gross-vogt@tugraz.at>
2023-11-23 11:31:12 +0100

    fix unit tests

--- src/ApiPlatform/DummyProvider.php
+++ src/ApiPlatform/DummyProvider.php
@@ -7,14 +7,25 @@ namespace Dbp\Relay\SublibraryBundle\ApiPlatform;
 use ApiPlatform\Metadata\CollectionOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProviderInterface;
+use Dbp\Relay\SublibraryBundle\Service\AlmaApi;
 
 class DummyProvider implements ProviderInterface
 {
+    /** @var AlmaApi */
+    private $api;
+
+    public function __construct(AlmaApi $api)
+    {
+        $this->api = $api;
+    }
+
     /**
      * @return array|null
      */
     public function provide(Operation $operation, array $uriVariables = [], array $context = [])
     {
+        $this->api->checkPermissions();
+
         if (!$operation instanceof CollectionOperationInterface) {
             return null;
         }
