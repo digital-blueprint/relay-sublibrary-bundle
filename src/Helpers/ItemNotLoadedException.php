91b333e328dc717437e7b784c9d29602f500bba9
Christoph Reiter <reiter.christoph@gmail.com>
2024-02-05 11:46:35 +0100

    cs-fixer

--- src/Helpers/ItemNotLoadedException.php
+++ src/Helpers/ItemNotLoadedException.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class ItemNotLoadedException extends HttpException
 {
-    public function __construct(?string $message = '', \Throwable $previous = null, array $headers = [], ?int $code = 0)
+    public function __construct(?string $message = '', ?\Throwable $previous = null, array $headers = [], ?int $code = 0)
     {
         parent::__construct(Response::HTTP_FAILED_DEPENDENCY, $message, $previous, $headers, $code);
     }
