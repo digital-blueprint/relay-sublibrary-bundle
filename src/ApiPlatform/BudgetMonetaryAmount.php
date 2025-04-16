2adf8c23af31505538a0cfff4a609a7443d5100c
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-16 15:37:44 +0200

    BudgetMonetaryAmount: port to PHP attributes

--- src/ApiPlatform/BudgetMonetaryAmount.php
+++ src/ApiPlatform/BudgetMonetaryAmount.php
@@ -4,13 +4,56 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\SublibraryBundle\ApiPlatform;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\Parameter;
 use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    shortName: 'LibraryBudgetMonetaryAmount',
+    description: 'A budget value of Sublibrary',
+    types: ['https://schema.org/MonetaryAmount'],
+    operations: [
+        new GetCollection(
+            uriTemplate: '/budget-monetary-amounts',
+            openapi: new Operation(
+                tags: ['Sublibrary'],
+                parameters: [
+                    new Parameter(
+                        name: 'sublibrary',
+                        in: 'query',
+                        description: 'Get all budget values of a sublibrary (ID of Sublibrary resource)',
+                        required: true,
+                        schema: ['type' => 'string'],
+                        example: '1190'
+                    ),
+                ]
+            ),
+            provider: BudgetMonetaryAmountProvider::class
+        ),
+        new Get(
+            uriTemplate: '/budget-monetary-amounts/{identifier}',
+            openapi: new Operation(
+                tags: ['Sublibrary']
+            ),
+            provider: BudgetMonetaryAmountProvider::class
+        ),
+    ],
+    routePrefix: '/sublibrary',
+    normalizationContext: [
+        'groups' => ['LibraryBudgetMonetaryAmount:output'],
+        'jsonld_embed_context' => true,
+    ]
+)]
 class BudgetMonetaryAmount
 {
     /**
      * @var string
      */
+    #[ApiProperty(identifier: true)]
     private $identifier;
 
     /**
@@ -22,12 +65,14 @@ class BudgetMonetaryAmount
     /**
      * @var float
      */
+    #[ApiProperty(iris: ['http://schema.org/value'])]
     #[Groups(['LibraryBudgetMonetaryAmount:output'])]
     private $value;
 
     /**
      * @var string
      */
+    #[ApiProperty(iris: ['http://schema.org/currency'])]
     #[Groups(['LibraryBudgetMonetaryAmount:output'])]
     private $currency;
 
