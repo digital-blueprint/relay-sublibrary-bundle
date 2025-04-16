fff422ed9374fda6e71af56a46a11fcc9a674e5f
Christoph Reiter <reiter.christoph@gmail.com>
2025-04-16 15:51:49 +0200

    BookOrderItem: port to PHP attributes

--- src/ApiPlatform/BookOrderItem.php
+++ src/ApiPlatform/BookOrderItem.php
@@ -4,33 +4,61 @@ declare(strict_types=1);
 
 namespace Dbp\Relay\SublibraryBundle\ApiPlatform;
 
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\OpenApi\Model\Operation;
 use Symfony\Component\Serializer\Annotation\Groups;
 
+#[ApiResource(
+    shortName: 'LibraryBookOrderItem',
+    description: 'A book order item from the library',
+    types: ['http://schema.org/OrderItem'],
+    operations: [
+        new Get(
+            uriTemplate: '/book-order-items/{identifier}',
+            openapi: new Operation(
+                tags: ['Sublibrary']
+            ),
+            provider: DummyProvider::class
+        ),
+    ],
+    routePrefix: '/sublibrary',
+    normalizationContext: [
+        'groups' => ['LibraryBookOrder:output'],
+        'jsonld_embed_context' => true,
+    ]
+)]
 class BookOrderItem
 {
+    #[ApiProperty(identifier: true)]
     private $identifier;
 
     /**
      * @var Book
      */
+    #[ApiProperty(iris: ['http://schema.org/OrderItem'])]
     #[Groups(['LibraryBookOrder:output'])]
     private $orderedItem;
 
     /**
      * @var ParcelDelivery
      */
+    #[ApiProperty(iris: ['http://schema.org/ParcelDelivery'])]
     #[Groups(['LibraryBookOrder:output'])]
     private $orderDelivery;
 
     /**
      * A BookOrderItem usually doesn't have a price, but we are assigning one, so we don't need to add another BookOffer layer to our book loan list.
      */
+    #[ApiProperty(iris: ['http://schema.org/price'])]
     #[Groups(['LibraryBookOrder:output'])]
     private $price;
 
     /**
      * A BookOrderItem usually doesn't have a priceCurrency, but we are assigning one, so we don't need to add another BookOffer layer to our book loan list.
      */
+    #[ApiProperty(iris: ['http://schema.org/priceCurrency'])]
     #[Groups(['LibraryBookOrder:output'])]
     private $priceCurrency;
 
