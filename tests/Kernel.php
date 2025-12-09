8688c6787bc6cd12db5da6980b0665cd378ce78d
Christoph Reiter <reiter.christoph@gmail.com>
2025-12-09 13:04:34 +0100

    Add support for Symfony 7.4

--- tests/Kernel.php
+++ tests/Kernel.php
@@ -47,7 +47,7 @@ class Kernel extends BaseKernel
         $container->import('@DbpRelayCoreBundle/Resources/config/services_test.yaml');
         $container->extension('framework', [
             'test' => true,
-            'secret' => '',
+            'secret' => 'something',
             'annotations' => false,
         ]);
 
