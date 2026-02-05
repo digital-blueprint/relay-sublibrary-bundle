fdbc962a9c5ee05d5a6a8dfa850c063aa13c1529
Christoph Reiter <reiter.christoph@gmail.com>
2026-02-05 12:09:28 +0100

    alma: add support for the 2026-02 release
    "Holdings Details::Holdings ID" was renamed to "Physical Item Details::Holdings Id"

--- src/Service/AlmaApi.php
+++ src/Service/AlmaApi.php
@@ -831,7 +831,7 @@ class AlmaApi implements LoggerAwareInterface
             $mmsId = $values['Bibliographic Details::MMS Id'];
             $loanId = $values['Loan Details::Item Loan Id'];
             $itemId = $values['Physical Item Details::Physical Item Id'] ?? $values['Physical Item Details::Item Id'];
-            $holdingId = $values['Holdings Details::Holdings ID'] ?? $values['Holding Details::Holding Id'];
+            $holdingId = $values['Physical Item Details::Holdings Id'] ?? ($values['Holdings Details::Holdings ID'] ?? $values['Holding Details::Holding Id']);
 
             if ($mmsId === '' || $loanId === '' || $itemId === '' || $holdingId === '') {
                 continue;
@@ -1527,7 +1527,7 @@ class AlmaApi implements LoggerAwareInterface
         foreach ($rows as $row) {
             $values = AlmaUtils::mapRowColumns($row, $mapping);
             $mmsId = $values['Bibliographic Details::MMS Id'];
-            $holdingId = $values['Holdings Details::Holdings ID'] ?? $values['Holding Details::Holding Id'];
+            $holdingId = $values['Physical Item Details::Holdings Id'] ?? ($values['Holdings Details::Holdings ID'] ?? $values['Holding Details::Holding Id']);
             $itemId = $values['Physical Item Details::Physical Item Id'] ?? $values['Physical Item Details::Item Id'];
 
             if ($mmsId === '' || $holdingId === '' || $itemId === '') {
